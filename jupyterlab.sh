e3e6a6e97101eab34505157450a1770fdfca72b1
ameliehuebner <amelie.huebner@uni-jena.de>
2026-08-04 14:07:01 +0200

    update

--- jupyterlab.sh
+++ jupyterlab.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=jupyterlab
 #SBATCH --partition=interactive
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=32G
 #SBATCH --time=04:00:00
 #-- Activate your conda environment which contains the jupyter package
 source /home/$USER/.bashrc
-conda activate pcp          # NOTE: you have to create an environment with the `jupyter` package
+conda activate plastchem        # NOTE: you have to create an environment with the `jupyter` package
 #-- Jupyter is served at a dedicated port. To not conflict with other users we choose a random free port
 read LOWERPORT UPPERPORT < /proc/sys/net/ipv4/ip_local_port_range
 while :
