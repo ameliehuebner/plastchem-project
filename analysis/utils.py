302548ddfea2905ec90a8b2b2fe7c5fa16300764
ameliehuebner <amelie.huebner@uni-jena.de>
2026-08-29 18:28:34 +0200

    update

--- analysis/utils.py
+++ analysis/utils.py
@@ -135,7 +135,7 @@ def extract_feature_vecs(data, n, vars=None):
     feature_vectors = pca.fit_transform(X)
     return feature_vectors, pca.explained_variance_ratio_
  
-########################################################################## RDKit utils
+########## rdkit utils ##########
 import pandas as pd
 import numpy as np
 import math
@@ -170,4 +170,48 @@ def calculate_mfps(data: pd.DataFrame, column_name: str):
 
 
 
+########## focal loss implementation ##########
+# from https://github.com/jhwjhw0123/Imbalance-XGBoost/blob/master/imxgboost/focal_loss.py
+# paper: https://doi.org/10.1016/j.patrec.2020.05.035
+class Focal_Binary_Loss:
+    '''
+    The class of focal loss, allows the users to change the gamma parameter
+    '''
 
+    def __init__(self, gamma_indct):
+        '''
+        :param gamma_indct: The parameter to specify the gamma indicator
+        '''
+        self.gamma_indct = gamma_indct
+
+    def robust_pow(self, num_base, num_pow):
+        # numpy does not permit negative numbers to fractional power
+        # use this to perform the power algorithmic
+
+        return np.sign(num_base) * (np.abs(num_base)) ** (num_pow)
+
+    def focal_binary_object(self, pred, dtrain):
+        gamma_indct = self.gamma_indct
+        # retrieve data from dtrain matrix
+        label = dtrain.get_label()
+        # compute the prediction with sigmoid
+        sigmoid_pred = 1.0 / (1.0 + np.exp(-pred))
+        # gradient
+        # complex gradient with different parts
+        g1 = sigmoid_pred * (1 - sigmoid_pred)
+        g2 = label + ((-1) ** label) * sigmoid_pred
+        g3 = sigmoid_pred + label - 1
+        g4 = 1 - label - ((-1) ** label) * sigmoid_pred
+        g5 = label + ((-1) ** label) * sigmoid_pred
+        # combine the gradient
+        grad = gamma_indct * g3 * self.robust_pow(g2, gamma_indct) * np.log(g4 + 1e-9) + \
+               ((-1) ** label) * self.robust_pow(g5, (gamma_indct + 1))
+        # combine the gradient parts to get hessian components
+        hess_1 = self.robust_pow(g2, gamma_indct) + \
+                 gamma_indct * ((-1) ** label) * g3 * self.robust_pow(g2, (gamma_indct - 1))
+        hess_2 = ((-1) ** label) * g3 * self.robust_pow(g2, gamma_indct) / g4
+        # get the final 2nd order derivative
+        hess = ((hess_1 * np.log(g4 + 1e-9) - hess_2) * gamma_indct +
+                (gamma_indct + 1) * self.robust_pow(g5, gamma_indct)) * g1
+
+        return grad, hess
